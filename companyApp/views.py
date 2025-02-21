97653ec53a3ec3ef40980f431909ac6695b6ece2
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-02-21 15:28:31 +0200

    Update copmanyApp Views Added newly implemented method `addShareholder`
    Renamed `companyCreation` -> `addCompany`
    To be in line with `addShareholder`.
    
    Broke up adding shareholder to separet method.

--- companyApp/views.py
+++ companyApp/views.py
@@ -1,48 +1,53 @@
-from django.shortcuts import render
-from django.shortcuts import get_object_or_404
-from django.shortcuts import redirect
-
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
 
 from .models import Company
 
 from .forms import CompanyForm 
-from .forms import ShareholderFormSet
+from .forms import ShareholderForm
 
 def index(request):
-    query = request.GET.get('q', '')
-    companies = []
+    query = request.GET.get('q', '').strip()
+    companies = Company.objects.all()
+
     if query:
         companies = Company.objects.filter(
-            Q(name__icontains=query) | Q(registration_code__icontains=query) |
-            Q(shareholders__name__icontains=query) | Q(shareholders__registry_code_or_id__icontains=query)
+            Q(name__icontains=query) | Q(registration_code__icontains=query)
         ).distinct()
 
     return render(request, 'index.html', {'companies': companies, 'query': query})
+
 
 def companyData(request, company_id):
     company = get_object_or_404(Company, id=company_id)
     shareholders = company.shareholders.all()
     return render(request, 'company_data.html', {'company': company, 'shareholders': shareholders})
 
-def companyCreation(request):
+def addCompany(request):
     if request.method == 'POST':
-        company_form = CompanyForm(request.POST)
-        shareholder_formset = ShareholderFormSet(request.POST)
+        companyForm = CompanyForm(request.POST)
 
-        if company_form.is_valid() and shareholder_formset.is_valid():
-            company = company_form.save()
-            shareholders = shareholder_formset.save(commit=False)
-
-            for shareholder in shareholders:
-                shareholder.company = company
-                shareholder.is_founder = True
-                shareholder.save()
-
+        if companyForm.is_valid():
+            company = companyForm.save()
             return redirect('company_data', company_id=company.id)
 
     else:
         company_form = CompanyForm()
-        shareholder_formset = ShareholderFormSet()
 
-    return render(request, 'company_creation.html', {'company_form': company_form, 'shareholder_formset': shareholder_formset})
+    return render(request, 'company_creation.html', {'company_form': company_form})
+
+def addShareholder(request, company_id):
+    company = get_object_or_404(Company, id=company_id)
+
+    if request.method == 'POST':
+        shareholder_form = ShareholderForm(request.POST)
+        if shareholder_form.is_valid():
+            shareholder = shareholder_form.save(commit=False)
+            shareholder.company = company
+            shareholder.save()
+            return redirect('company_data', company_id=company.id)
+
+    else:
+        shareholder_form = ShareholderForm()
+
+    return render(request, 'add_shareholder.html', {'shareholder_form': shareholder_form, 'company': company})
