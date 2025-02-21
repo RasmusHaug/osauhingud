e7b5090c4de51650f7a0fad45748b0443c7c1483
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-02-21 15:26:34 +0200

    Update companyApp Urls Added URL to newly implemented `add_shareholder` page.

--- companyApp/urls.py
+++ companyApp/urls.py
@@ -5,5 +5,6 @@ from companyApp import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("osaühing/<int:company_id>/", views.companyData, name="company_data"),
-    path("osaühing/loo-uus", views.companyCreation, name="company_creation"),
+    path("osaühing/loo-uus", views.addCompany, name="company_creation"),
+    path('company/<int:company_id>/add_shareholder/', views.addShareholder, name='add_shareholder'),
 ]
