50a93117581b03a45fd8afb786f4497bd30d46a4
RasmusHaug <97478497+RasmusHaug@users.noreply.github.com>
2025-02-23 20:16:19 +0200

    Update companyApp admin Update admin page to allow visuals to see database data and modify it.

--- companyApp/admin.py
+++ companyApp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from companyApp.models import Company, Shareholder
+
 # Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registration_code', 'date_of_establishment', 'total_capital')
+    search_fields = ('name', 'registration_code')
+    list_filter = ('date_of_establishment',)
+
+# Define a model admin class for Shareholder
+class ShareholderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registry_code_or_id', 'shareholder_type', 'share_size', 'company')
+    search_fields = ('name', 'registry_code_or_id')
+    list_filter = ('shareholder_type',)
+
+# Register the models with their respective admin classes
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Shareholder, ShareholderAdmin)
